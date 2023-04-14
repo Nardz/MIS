9c4167452148163dc473b221263dbfae165da3b4
Nardz <leonardryan.paglinawan@gmail.com>
2023-04-14 15:52:43 +0800

    retrieve employees

--- src/scenes/users/index.jsx
+++ src/scenes/users/index.jsx
@@ -3,67 +3,192 @@ import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
 import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import { Box, Typography, useTheme } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Chip from '@mui/material/Chip';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
-import { Link } from 'react-router-dom';
+import { Link ,useNavigate} from 'react-router-dom';
 import Header from '../../components/header/Header';
 import { mockDataContacts } from '../../data/mockData';
 import { tokens } from '../../theme';
+import { useEffect } from 'react';
+import axiosInstance from '../../api/axios';
+import { useSnackbar } from 'notistack';
+import { useState } from 'react';
 
 const Users = () => {
 	const theme = useTheme();
 	const colors = tokens(theme.palette.mode);
+	const { enqueueSnackbar } = useSnackbar();
+	const navigate = useNavigate();
+	const token = sessionStorage.getItem("token")
+	const config = {
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${token}`
+		}
+	};
+	const [emplist, setEmplist] = useState([])
+	//const [Pic, setPic] = useState('');
 
+	const getEmpList = async () =>{
+			await axiosInstance.get('Employee/Emplist',config)
+			.then((res)=>{
+				//console.log(res.data)
+				setEmplist(res.data)
+			//	console.log(emplist)
+
+			}).catch((err) =>{
+				console.log(err)
+					const variant = 'error';
+					enqueueSnackbar('Unable to get employee list ', {
+					variant,
+				});
+				if (err.code == 'ERR_NETWORK' || err.response.status == 401) {
+						sessionStorage.clear()
+						navigate('/login');
+				}
+			})
+	}
+
+	useEffect(() => {
+		getEmpList()
+	},[])
+
+
+
+	
+
+	// function checkIfImageExists(url, callback) {
+	// 	const img = new Image();
+	// 	img.src = url;
+		
+	// 	if (img.complete) {
+	// 		callback(true);
+	// 	} else {
+	// 		img.onload = () => {
+	// 			callback(true);
+	// 		};
+			
+	// 		img.onerror = () => {
+	// 			callback(false);
+	// 		};
+	// 	}
+	// }
+	
+		
+
+		// const getImage =  (empId) =>{
+
+		// 	const yourImage = process.env.PUBLIC_URL + `assets/user/${empId}.png`;
+	     const defaultImage = process.env.PUBLIC_URL + `assets/user/user.png`;
+
+		// checkIfImageExists(yourImage, (exists) => {
+		// 	if (exists) {
+		// 		setPic(yourImage)
+		// 	} else {
+		// 		setPic(defaultImage)
+		// 	}
+		// 	});
+		// }
 	const columns = [
-		{ field: 'id', headerName: 'ID', flex: 0.5 },
+		 //{ field: 'empId', headerName: 'ID', flex: 0.5 },
 		{
 			field: 'name',
 			headerName: 'Name',
-			flex: 1,
+			flex: 2,
 			cellClassName: 'name-column--cell',
 			headerAlign: 'center',
-		},
-
-		{
-			field: 'status',
-			headerName: 'Status',
-			type: 'text',
-			headerAlign: 'center',
 			align: 'center',
-			renderCell: ({ row: { status } }) => {
-				return (
-					<>
-						<Chip
-							color={status === 'active' ? 'success' : 'error'}
-							label={status}
-							variant="outlined"
-						/>
-					</>
-				);
+			renderCell : (params) =>{
+					return (
+							<>
+							
+							<Chip
+									 avatar={
+									 <Avatar alt="Natacha" src={defaultImage} />
+									}
+									label={params.row.name }
+									variant="outlined"
+									
+								/>
+							
+							</>
+					);
 			},
 		},
 
 		{
-			field: 'accessLevel',
+			field: 'stats',
+			headerName: 'Status',
+			type: 'number',
+			headerAlign: 'center',
+			align: 'center',
+			// renderCell: ({ row: { status } }) => {
+			// 	return (
+			// 		<>
+					
+			// 			<Chip
+			// 				color={status === "1" ? 'success' : 'error'}
+			// 				label={status === "1" ? 'Active' : 'Inactive'}
+			// 				variant="outlined"
+			// 			/>
+			// 		</>
+			// 	);
+			// },
+			renderCell : (params) =>{
+				return (
+							<>
+							
+								<Chip
+									color={params.row.stats === 1 ? 'success' : 'error'}
+									label={params.row.stats === 1 ? 'Active' : 'Inactive'}
+									variant="outlined"
+								/>
+							</>
+						);
+			},
+		},
+
+		{
+			field: 'description',
 			headerName: 'Access Level',
 			flex: 1,
 			headerAlign: 'center',
 			align: 'center',
-			renderCell: ({ row: { userType } }) => {
+			// renderCell: ({ row: { userType } }) => {
+			// 	return (
+			// 		<>
+			// 			<Chip
+			// 				avatar={
+			// 					userType === 'Super Admin' ? (
+			// 						<AdminPanelSettingsOutlinedIcon />
+			// 					) : userType === 'Admin' ? (
+			// 						<SecurityOutlinedIcon />
+			// 					) : (
+			// 						userType === 'Read Only' && <LockOpenOutlinedIcon />
+			// 					)
+			// 				}
+			// 				label={userType}
+			// 			/>
+			// 		</>
+			// 	);
+			// },
+
+			renderCell : (params) =>{
 				return (
 					<>
 						<Chip
 							avatar={
-								userType === 'admin' ? (
+								params.row.description === 'Super Admin' ? (
 									<AdminPanelSettingsOutlinedIcon />
-								) : userType === 'manager' ? (
+								) : params.row.description === 'Admin' ? (
 									<SecurityOutlinedIcon />
 								) : (
-									userType === 'user' && <LockOpenOutlinedIcon />
+									params.row.description === 'Read Only' && <LockOpenOutlinedIcon />
 								)
 							}
-							label={userType}
+							label={params.row.description}
 						/>
 					</>
 				);
@@ -71,22 +196,25 @@ const Users = () => {
 		},
 
 		{
-			field: 'view',
+			field: 'key',
 			headerName: 'View Details',
 			type: 'text',
 			headerAlign: 'center',
 			align: 'center',
-			renderCell: ({ row: { id } }) => {
+			//renderCell: ({ row: { id } }) => {	
+			renderCell: (params) => {
 				return (
 					<>
 						<Button
 							variant="contained"
-							id={id}
+							//id={id}
+							id={params.row.key}
 							startIcon={<VisibilityIcon />}
 							color="info"
 							sx={{ fontWeight: 'bold', borderRadius: '100px' }}
 							component={Link}
-							to={`/employees/${id}`}
+							//to={`/employees/${id}`}
+							to={`/employees/${params.row.key}`}
 						>
 							VIEW
 						</Button>
@@ -97,6 +225,7 @@ const Users = () => {
 	];
 
 	return (
+		
 		<Box m="20px">
 			<Header title="Employees" subtitle="List of Employees" />
 			<Box
@@ -134,10 +263,12 @@ const Users = () => {
 					mb: 15,
 				}}
 			>
+			
 				<DataGrid
-					rows={mockDataContacts}
+					rows={emplist}
 					columns={columns}
 					components={{ Toolbar: GridToolbar }}
+					getRowId={(emplist) =>  emplist.empId}
 				/>
 			</Box>
 		</Box>
